341789c72eb9a945e78f5999a8a6f7d8820a1664
Akhil Remashan <akhil@softwiztech.com>
2022-01-12 23:14:06 +0530

    js1201

--- js/station-counter/main.js
+++ js/station-counter/main.js
@@ -1,5 +1,49 @@
 // document.getElementById("count-el").innerText = 5;
 
-let myAge = 30;
+// VARIABLES
+// let myAge = 29;
+// let humanDogRatio = 7;
+// myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
 
-console.log(myAge);
+// 
+// 
+// 
+// INCREMENT + DECREMENT
+// let bonusPoints = 50;
+// bonusPoints = bonusPoints + 100;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 25;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 70;
+// console.log(bonusPoints);
+// 
+// 
+// ONCLICK FUNCTION
+// function increment() {
+//     console.log("clicked!")
+// }
+// 
+// 
+// 
+// FUNCTION
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+// 
+// function lapsum() {
+//     sum = lap1 + lap2 + lap3;
+//     console.log(sum);
+// }
+// 
+// lapsum();
+// 
+// 
+// 
+// SUBWAY COUNTER
+let psgcount = 0;
+function increment() {
+    psgcount = psgcount + 1;
+    console.log(psgcount);
+    document.getElementById("count-el").innerText = psgcount;
+}
