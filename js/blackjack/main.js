57317db8d0f2b63344353ecb80b1cf11078fb1f8
Akhil Remashan <sudonowww@gmail.com>
2022-01-23 20:52:11 +0530

    js2301

--- js/blackjack/main.js
+++ js/blackjack/main.js
@@ -1,8 +1,36 @@
 
 
-let firstCard = 9;
-let secondCard = 3;
+let firstCard = 20;
+let secondCard = 1;
 
 let sum =  firstCard + secondCard;
 
-console.log(sum);
+let hasBlackJack = false;
+let isAlive = true;
+
+message = "";
+
+// console.log(sum);
+
+function startgame() {
+
+    if (sum < 21) {
+
+        message = "Do you want to draw a new card? 🙂 ";
+    
+    } else if (sum === 21) {
+    
+        message = "Woohoo! You've got a Blackjack! 🥳 ";
+        hasBlackJack = true;
+    
+    } else {
+    
+        message = "You're out of the game ☹️ ";
+        isAlive = false;
+    
+    }
+    
+    console.log(message);
+
+}
+
