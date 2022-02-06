a7b518e91bcfc6abf365adab2c3cdf84bb1bbd51
Akhil Remashan <sudonowww@gmail.com>
2022-02-06 21:00:38 +0530

    ex0602

--- express/apig-one/app.js
+++ express/apig-one/app.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const app = express();
 
+
+// Middlewares
+app.use('/posts', () => {
+    // console.log("This is a middleware running");
+});
+
+
 // Route
 app.get('/', (req, res) => {
 
     res.send("we are home");
+
+});
+
+app.get('/posts', (req,res) => {
+
+    res.send("We are posts");
 
 });
 
