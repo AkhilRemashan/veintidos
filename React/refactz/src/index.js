fa4ef01155787048ab776dde0e83c78b27218ac6
Akhil Remashan <sudonowww@gmail.com>
2022-03-11 23:10:31 +0530

    re1103

--- React/refactz/src/index.js
+++ React/refactz/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// COMPONENTS
+
+
 import Navbar from './components/Navbar'
 import Main from './components/Main'
-// END COMPONENTS
 
 
 
