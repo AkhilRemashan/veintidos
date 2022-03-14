3151ffef68af3b04d0f31fdae589c54555a09488
Akhil Remashan <sudonowww@gmail.com>
2022-03-14 23:41:50 +0530

    re1403

--- React/refactz/src/index.js
+++ React/refactz/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-
-
-import Navbar from './components/Navbar'
-import Main from './components/Main'
-
 
 
 ReactDOM.render(
   <>
-    <Navbar />
-    <Main />
+    <App />
   </>,
   document.getElementById('root')
 );
